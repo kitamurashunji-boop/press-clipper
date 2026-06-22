1a1f071ddbd81d0f324fb2ed4227264d07808d46
kitamurashunji-boop <kitamura.shunji@gmail.com>
2026-06-22 22:17:03 +0900

    Use env.bat for API keys (gitignored)

--- start.bat
+++ start.bat
@@ -1,5 +1,6 @@
 @echo off
 cd /d "%~dp0"
+if exist env.bat call env.bat
 echo Press Clipper を起動中...
 echo http://localhost:8502 をブラウザで開いてください
 python -m uvicorn server:app --host 0.0.0.0 --port 8502 --reload
