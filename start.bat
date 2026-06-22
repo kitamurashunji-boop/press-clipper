8c63bc1fa4c21c8e29da675b6367e75a02eca13c
kitamurashunji-boop <kitamura.shunji@gmail.com>
2026-06-22 22:16:17 +0900

    Use env.bat for API keys (gitignored)

--- start.bat
+++ start.bat
@@ -1,8 +1,6 @@
 @echo off
 cd /d "%~dp0"
-set ANTHROPIC_API_KEY=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
-set GOOGLE_API_KEY=AIzaSyBrmgwvo3Rs154MqTauwNRpPNGHrIDtR6E
-set GOOGLE_CX=26fdb0bfd0a7943ec
+if exist env.bat call env.bat
 echo Press Clipper を起動中...
 echo http://localhost:8502 をブラウザで開いてください
 python -m uvicorn server:app --host 0.0.0.0 --port 8502 --reload
